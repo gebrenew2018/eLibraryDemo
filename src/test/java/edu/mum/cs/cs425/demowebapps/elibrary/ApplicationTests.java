fd8c2df1f335d8d897e6788ef0f46e78f2d35fa9
gebrenew2018 <gebrenew2012@gmail.com>
2020-03-03 22:31:05 -0600

    demo of eLibrary

---
+++ src/test/java/edu/mum/cs/cs425/demowebapps/elibrary/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
